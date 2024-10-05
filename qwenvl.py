4dfe49e4ce4a67193979a61025b22ded978caaf7
share121 <2854631158@qq.com>
2024-10-05 21:17:29 +0800

    f

--- qwenvl.py
+++ qwenvl.py
@@ -53,7 +53,7 @@ inputs = processor(
     padding=True,
     return_tensors="pt",
 )
-inputs = inputs.to("cuda")
+inputs = inputs.to("cpu")
 
 # Inference: Generation of the output
 generated_ids = model.generate(**inputs, max_new_tokens=128)

5a79268e865caa3c5b5328a0f7825c7586b4af96
share121 <2854631158@qq.com>
2024-10-20 11:13:19 +0800

    f

--- actions/save-discussion.ts
+++
@@ -1,6 +0,0 @@
-import Event from "./event.json" with { type: "json" };
-const event: typeof Event = JSON.parse(Deno.env.get("event")!);
-Deno.writeTextFile(
-  `discussions/${event.discussion.number}-${Date.now()}.json`,
-  JSON.stringify(event),
-);

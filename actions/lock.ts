379b451b19b650e4437fdc406789c4f2fdeae475
share121 <2854631158@qq.com>
2024-11-02 20:51:28 +0800

    f

--- actions/lock.ts
+++ actions/lock.ts
@@ -1,13 +1,47 @@
 import { App, Octokit } from "npm:octokit";
 
-const actionId = Deno.env.get("actionId")!,
+const runNumber = +Deno.env.get("runNumber")!,
   githubToken = Deno.env.get("githubToken")!,
   repo = Deno.env.get("repo")!,
   [owner, repoName] = repo.split("/");
 
+type status =
+  | "completed"
+  | "action_required"
+  | "cancelled"
+  | "failure"
+  | "neutral"
+  | "skipped"
+  | "stale"
+  | "success"
+  | "timed_out"
+  | "in_progress"
+  | "queued"
+  | "requested"
+  | "waiting"
+  | "pending";
+
+// 得到 workflow id
 const octokit = new Octokit({ auth: githubToken });
-const list = await octokit.rest.actions.listWorkflowRunsForRepo({
+const workflow = await octokit.rest.actions.getWorkflowRun({
   owner,
   repo: repoName,
+  run_id: runNumber,
 });
-console.log(list);
+const workflowId = workflow.data.workflow_id;
+
+// 获取 workflow run 列表
+const list = await octokit.rest.actions.listWorkflowRuns({
+  owner,
+  repo: repoName,
+  workflow_id: workflowId,
+});
+const data = list.data.workflow_runs;
+for (const i of data) {
+  if (!["in_progress"].includes(i.status ?? "")) continue;
+  const updatedAt = new Date(i.updated_at);
+  if (updatedAt.getTime() >= Date.now()) continue;
+  octokit.hook("workflow_job", (e) => {
+    console.log(e);
+  }, {});
+}

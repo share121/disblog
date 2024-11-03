77ce59e6089b636e97acb5adb4aaea2170f34907
share121 <2854631158@qq.com>
2024-11-03 10:09:46 +0800

    f

---
+++ ai-tools/utility/visit-website.ts
@@ -0,0 +1,29 @@
+export const option = {
+  type: "function",
+  function: {
+    parameters: {
+      type: "object",
+      properties: {
+        urls: {
+          type: "string",
+          description: "要访问的网页地址，多个地址用换行分隔",
+        },
+      },
+      required: ["urls"],
+    },
+    name: "visit-website",
+    description: "访问网页",
+  },
+};
+
+export async function execute(args: { [key: string]: string }) {
+  const urls = args.urls.split("\n");
+  const p = urls.map(async (url) => {
+    const r = await fetch(url);
+    if (r.headers.get("content-type") === "text/html") {
+      const text = await r.text();
+      return text;
+    }
+  });
+  return JSON.stringify(await Promise.allSettled(p));
+}

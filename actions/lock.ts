0d7f3b2e1bdc31e1ea04a8ae02bc91d5ff689c7d
share121 <2854631158@qq.com>
2024-11-09 22:40:49 +0800

    f

---
+++ actions/lock.ts
@@ -0,0 +1,46 @@
+import { Octokit } from "npm:octokit";
+
+const runNumber = +Deno.env.get("runNumber")!,
+  repo = Deno.env.get("repo")!,
+  [owner, repoName] = repo.split("/");
+
+type status =
+  | "completed"
+  | "action_required"
+  | "cancelled"
+  | "failure"
+  | "neutral"
+  | "skipped"
+  | "stale"
+  | "success"
+  | "timed_out"
+  | "in_progress"
+  | "queued"
+  | "requested"
+  | "waiting"
+  | "pending";
+
+// 得到 workflow id
+const octokit = new Octokit();
+const workflow = await octokit.rest.actions.getWorkflowRun({
+  owner,
+  repo: repoName,
+  run_id: runNumber,
+});
+const workflowId = workflow.data.workflow_id;
+
+// 获取 workflow run 列表
+const list = await octokit.rest.actions.listWorkflowRuns({
+  owner,
+  repo: repoName,
+  workflow_id: workflowId,
+});
+const data = list.data.workflow_runs;
+for (const i of data) {
+  if (!["in_progress"].includes(i.status ?? "")) continue;
+  const updatedAt = new Date(i.updated_at);
+  if (updatedAt.getTime() >= Date.now()) continue;
+  octokit.hook("workflow_job", (e) => {
+    console.log(e);
+  }, {});
+}

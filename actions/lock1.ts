fb92723aa41ce382d24654e262a9cf4d96e18c08
share121 <2854631158@qq.com>
2024-11-10 09:01:25 +0800

    f

--- actions/lock1.ts
+++ actions/lock1.ts
@@ -7,19 +7,33 @@ const runNumber = +Deno.env.get("runNumber")!,
 
 const octokit = new Octokit();
 
-const f = await octokit.request(
-  "GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs",
+const workflows = await octokit.request(
+  "GET /repos/{owner}/{repo}/actions/workflows",
   {
     owner: "share121",
     repo: "disblog",
-    workflow_id: workflowName,
     headers: {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   },
 );
 
-console.log(f.data);
+const workflowId =
+  workflows.data.workflows.find((e) => e.name === workflowName)!.id;
+
+const f = await octokit.request(
+  "GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs",
+  {
+    owner: "share121",
+    repo: "disblog",
+    workflow_id: workflowId,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  },
+);
+
+console.log(f.data.workflow_runs);
 
 for (const i of f.data.workflow_runs) {
   if (!["in_progress"].includes(i.status ?? "")) continue;
